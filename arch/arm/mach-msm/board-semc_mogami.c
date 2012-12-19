ca05edf1dc75bd6c9937bec793f5e39c2e846a10
irii-soft <iriijo@googlemail.de>
2012-12-19 17:26:02 +0100

    CleanUp Part 1

--- arch/arm/mach-msm/board-semc_mogami.c
+++ arch/arm/mach-msm/board-semc_mogami.c
@@ -193,7 +193,6 @@
 #define MSM_FB_SIZE             (864 * 480 * 4 * 2) + MSM_HDMI_SIZE
 #endif /*CONFIG_FB_MSM_TRIPLE_BUFFER*/
 
-#define MSM_PMEM_CAMERA_SIZE    0x0000000
 #define MSM_PMEM_ADSP_SIZE      0x2F00000
 #define PMEM_KERNEL_EBI1_SIZE   0x600000
 
@@ -4495,14 +4494,6 @@ static void __init pmem_adsp_size_setup(char **p)
 
 __early_param("pmem_adsp_size=", pmem_adsp_size_setup);
 
-static unsigned pmem_camera_size = MSM_PMEM_CAMERA_SIZE;
-static void __init pmem_camera_size_setup(char **p)
-{
-	pmem_camera_size = memparse(*p, p);
-}
-
-__early_param("pmem_camera_size=", pmem_camera_size_setup);
-
 static unsigned pmem_kernel_ebi1_size = PMEM_KERNEL_EBI1_SIZE;
 static void __init pmem_kernel_ebi1_size_setup(char **p)
 {
@@ -4538,15 +4529,6 @@ static void __init msm7x30_allocate_memory_regions(void)
 		android_pmem_adsp_pdata.start = __pa(addr);
 		android_pmem_adsp_pdata.size = size;
 		pr_info("allocating %lu bytes at %p (%lx physical) for adsp "
-			"pmem arena\n", size, addr, __pa(addr));
-	}
-
-	size = pmem_camera_size;
-	if (size) {
-		addr = alloc_bootmem(size);
-		android_pmem_camera_pdata.start = __pa(addr);
-		android_pmem_camera_pdata.size = size;
-		pr_info("allocating %lu bytes at %p (%lx physical) for camera "
 			"pmem arena\n", size, addr, __pa(addr));
 	}
 
